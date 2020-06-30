0d36ca115ed4a6dcf45c39e3994fa46a20d72719
Rayna Weeden <imagineskies@protonmail.com>
2020-06-30 16:38:26 -0400

    Major Update

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var scrollAmount = deviceHeight - navHeight;
 var mainNavPos = document.getElementById('mainNav').offsetTop;
 var temp = '0';
 var farm = 0;
+var screenHeight = $(window).width();
 console.log(temp)
 
 /*____________________________________________________________________________________*/
@@ -14,6 +15,7 @@ console.log(temp)
 const body = document.getElementById('body');
 const mainNav = document.getElementById('mainNav');
 const navbarWrapper = document.getElementById('navbarWrapper');
+const updateMenu = document.getElementById('updateMenu');
 
 /*____________________________________________________________________________________*/
 // Functions
@@ -35,22 +37,27 @@ $(".sliding-link").click(function(e) {
 window.onscroll = function(){
   var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
   console.log(mainNavPos, scrollPos);
-  if (scrollPos >= 79){
+  if (scrollPos >= 79 && screenHeight >= 1188){
     navbarWrapper.classList.add('centerSlide');
     navbarWrapper.classList.remove('leftSlide');
     window.farm = '1';
     console.log(farm)
-  }  else if (farm=='1' && scrollPos<=79) {
+  }  else if (farm=='1' && scrollPos<=79 && screenHeight >= 1188) {
     console.log('ran')
     navbarWrapper.classList.remove('centerSlide');
     navbarWrapper.classList.add('leftSlide');
   }
-   if (scrollPos >= mainNavPos){
+   if (scrollPos >= mainNavPos && screenHeight >= 1188){
     mainNav.classList.add('sticky');
     window.farm = '1';
     console.log(farm)
-  } else if (farm=='1' && scrollPos<=mainNavPos) {
+  } else if (farm=='1' && scrollPos<=mainNavPos && screenHeight >= 1188) {
     console.log('ran')
     mainNav.classList.remove('sticky');
+  }
+  if (scrollPos >= 579 && screenHeight >= 1188) {
+    updateMenu.classList.add('sticky');
+  } else {
+    updateMenu.classList.remove('sticky');
   }
 }
