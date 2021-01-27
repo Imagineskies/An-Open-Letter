14e1eb2a640bd30e67828ee6819839c182153d20
Rayna Weeden <imagineskies@protonmail.com>
2021-01-27 11:18:30 -0500

    Major Update

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ var mainNavPos = document.getElementById('mainNav').offsetTop;
 var temp = '0';
 var farm = 0;
 var screenHeight = $(window).width();
-console.log(temp)
 
 /*____________________________________________________________________________________*/
 // Constants
@@ -16,15 +15,18 @@ const body = document.getElementById('body');
 const mainNav = document.getElementById('mainNav');
 const navbarWrapper = document.getElementById('navbarWrapper');
 const updateMenu = document.getElementById('updateMenu');
+const main = document.getElementById('main');
 
 /*____________________________________________________________________________________*/
 // Functions
 
 $(".sliding-link").click(function(e) {
-    e.preventDefault();
-    var aid = $(this).attr("href");
-    console.log(aid)
-    $('html,body').animate({scrollTop: $(aid).offset().top},1500);
+  e.preventDefault();
+  var aid = $(this).attr("href");
+  console.log(aid)
+  $('html,body').animate({
+    scrollTop: $(aid).offset().top
+  }, 1500);
 });
 
 
@@ -34,30 +36,24 @@ $(".sliding-link").click(function(e) {
 /*____________________________________________________________________________________*/
 // Event Listeners
 
-window.onscroll = function(){
+window.onscroll = function() {
   var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
   console.log(mainNavPos, scrollPos);
-  if (scrollPos >= 79 && screenHeight >= 1188){
-    navbarWrapper.classList.add('centerSlide');
-    navbarWrapper.classList.remove('leftSlide');
+  if (scrollPos >= 79 && screenHeight >= 1188) {
     window.farm = '1';
-    console.log(farm)
-  }  else if (farm=='1' && scrollPos<=79 && screenHeight >= 1188) {
-    console.log('ran')
-    navbarWrapper.classList.remove('centerSlide');
-    navbarWrapper.classList.add('leftSlide');
   }
-   if (scrollPos >= mainNavPos && screenHeight >= 1188){
+  if (scrollPos >= mainNavPos && screenHeight >= 1188) {
     mainNav.classList.add('sticky');
+    main.setAttribute('style', 'padding-top: 50px;');
     window.farm = '1';
-    console.log(farm)
-  } else if (farm=='1' && scrollPos<=mainNavPos && screenHeight >= 1188) {
-    console.log('ran')
+  } else if (farm == '1' && scrollPos <= mainNavPos && screenHeight >= 1188) {
     mainNav.classList.remove('sticky');
+    main.removeAttribute('style', 'padding-top: 50px;');
   }
-  if (scrollPos >= 579 && screenHeight >= 1188) {
-    updateMenu.classList.add('sticky');
+  if (scrollPos >= mainNavPos && screenHeight >= 1188) {
+   updateMenu.classList.replace('non-sticky', 'sticky');
+
   } else {
-    updateMenu.classList.remove('sticky');
+   updateMenu.classList.replace('sticky', 'non-sticky');
   }
 }
